7ca6244dba74f8d900a44b7cfb9c5c7a8e6cc839
aki <aquastar.dolphin@gmail.com>
2026-01-18 16:47:27 +0900

    [nochange] refactor comment

--- FilterPlugIn/Source/HSV/PISLICMain.cpp
+++ FilterPlugIn/Source/HSV/PISLICMain.cpp
@@ -25,8 +25,10 @@ static void Log(const std::string& msg) {
 #endif
 }
 
-/*--------------------------------------------------------------------------
-Copyright (c) 2025 Akihiro.Watanabe
+/* ---------------------------------------------------------------------------
+MIT License
+
+Copyright (c) 2026/2/14  Akihiro.Watanabe, (Mitobe Hikane)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -37,6 +39,14 @@ furnished to do so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 --------------------------------------------------------------------------- */
 
 // Property Keys
@@ -663,8 +673,6 @@ void TRIGLAV_PLUGIN_API TriglavPluginCall(TriglavPlugInInt* result, TriglavPlugI
 							}
 						}
 					}
-					
-					// ScopeBitmap destructors handle leaks if loop breaks early
 
 					Log("FilterRun Success");
 					*result = kTriglavPlugInCallResultSuccess;
